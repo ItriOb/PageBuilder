63704d0073d5be7f00d5d3bc379013b257fb184f
Itri Obanni <obanniitri@gmail.com>
2022-06-15 16:29:37 +0100

    second commit

---
+++ themes/booking/layouts/master/view.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title><?= $page->get('title') ?></title>
+    <link rel="stylesheet" href="<?= phpb_theme_asset('css/style.css') ?>" />
+    <link rel="stylesheet" href="<?= phpb_theme_asset('assets1/css/bootstrap.min.css') ?>" />
+    <link rel="stylesheet" href="<?= phpb_theme_asset('assets1/css/animate.min.css') ?>" />
+    <link rel="stylesheet" href="<?= phpb_theme_asset('assets1/css/magnific-popup.css') ?>" />
+    <link rel="stylesheet" href="<?= phpb_theme_asset('assets1/css/fontawesome-all.min.css') ?>" />
+    <link rel="stylesheet" href="<?= phpb_theme_asset('assets1/css/odometer.css') ?>" />
+    <link rel="stylesheet" href="<?= phpb_theme_asset('assets1/css/flaticon.css') ?>" />
+    <link rel="stylesheet" href="<?= phpb_theme_asset('assets1/css/slick.css') ?>" />
+    <link rel="stylesheet" href="<?= phpb_theme_asset('assets1/css/swiper-bundle.min.css') ?>" />
+    <link rel="stylesheet" href="<?= phpb_theme_asset('assets1/css/datepicker.css') ?>" />
+    <link rel="stylesheet" href="<?= phpb_theme_asset('assets1/css/default.css') ?>" />
+    <link rel="stylesheet" href="<?= phpb_theme_asset('assets1/css/style.css') ?>" />
+    <link rel="stylesheet" href="<?= phpb_theme_asset('assets1/css/responsive.css') ?>" />
+</head>
+<body>
+
+<?= $body ?>
+
+
+
+
+
+
+
+
+
+<script
+      data-cfasync="false"
+      src="<?= phpb_theme_asset('../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js') ?>"
+    ></script>
+    <script src="<?= phpb_theme_asset('assets1/js/vendor/jquery-3.6.0.min.js') ?>"></script>
+    <script src="<?= phpb_theme_asset('assets1/js/bootstrap.min.js') ?>"></script>
+    <script src="<?= phpb_theme_asset('assets1/js/jquery.magnific-popup.min.js') ?>"></script>
+    <script src="<?= phpb_theme_asset('assets1/js/jquery.odometer.min.js') ?>"></script>
+    <script src="<?= phpb_theme_asset('assets1/js/swiper-bundle.min.js') ?>"></script>
+    <script src="<?= phpb_theme_asset('assets1/js/slick.min.js') ?>"></script>
+    <script src="<?= phpb_theme_asset('assets1/js/jquery.appear.js') ?>"></script>
+    <script src="<?= phpb_theme_asset('assets1/js/wow.min.js') ?>"></script>
+    <script src="<?= phpb_theme_asset('assets1/js/datepicker.js') ?>"></script>
+    <script src="<?= phpb_theme_asset('assets1/js/main.js') ?>"></script>
+</body>
+</html>
